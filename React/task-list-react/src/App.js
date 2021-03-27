30fc9870621c750fcfb0d9d73e7b94e26d688fbc
Shantanu Korgaonkar <shantanukorgaonkar7@gmail.com>
2021-03-27 13:50:44 +0530

    project complete

--- React/task-list-react/src/App.js
+++ React/task-list-react/src/App.js
@@ -5,6 +5,8 @@ import Tasks from './components/Tasks'
 import AddForm from './components/AddForm'
 import About from './components/About'
 import {useState} from 'react'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+
 function App() {
   const [tasks,setTask]=useState([])
   const [showAddTask,setShowAddTask]=useState('false')
@@ -40,14 +42,22 @@ setTask(tasks.filter((task)=>{
 
 
   let name="Task Tracker"
+ 
   return (
+    <Router>
     <div className="container">
      <Header title={name} toggleButton={()=> { setShowAddTask(!showAddTask)}} showAdd={showAddTask}/>
-     { showAddTask && <AddForm taskAdd={addTask} />}
+     
+     <Route path="/" exact render={(props)=>(
+       <>
+       { showAddTask && <AddForm taskAdd={addTask} />}
      {tasks.length>0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />: "No tasks today"}
+       </>
+     )}/>
+     <Route path="/about" component={About} />
      <Footer />
-
     </div> 
+    </Router>
   );
 }
 
--- React/task-list-react/src/components/Header.js
+++ React/task-list-react/src/components/Header.js
@@ -1,15 +1,16 @@
 import PropTypes from 'prop-types'
 import Button from './Button'
-
+import {useLocation} from 'react-router-dom'
 
 
 
 
 const Header = ({title,toggleButton,showAdd}) => {
+    const location = useLocation()
     return (
         <header className="header">
             <h1>{title}</h1>
-            <Button text={showAdd?'close':'add'} buttonColor={showAdd?'red':'green'} onClick={toggleButton} />
+            {location.pathname === '/'&&<Button text={showAdd?'close':'add'} buttonColor={showAdd?'red':'green'} onClick={toggleButton} />}
           
           
 
