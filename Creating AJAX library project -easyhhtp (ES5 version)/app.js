3aa95d07e384f017e8517f902dfcc01c2a965f0c
Shantanu Korgaonkar <shantanukorgaonkar7@gmail.com>
2021-05-20 21:22:14 +0530

    library created and all http methods tested

--- Creating AJAX library project -easyhhtp (ES5 version)/app.js
+++ Creating AJAX library project -easyhhtp (ES5 version)/app.js
@@ -1,10 +1,73 @@
-const http = new easyHTTP // here easyHTTP is called. so this inside the func def will bind to http
-http.get()
-console.log(http)
 
 
-abc={
-    http:'xhr object',
-    get:'function'
-    
-}
+
+const lib = new easyHTTP // here easyHTTP is called. so this inside the func def will bind to lib. lib refers to an obj containing props and methods from easyHTTP def
+// lib refers to an object containing props of easyHTTP constructor.
+// http prop inside the constructor refers to the XHRHTTPRequest obj
+
+
+
+// GET Request
+
+
+
+// lib.get('http://jsonplaceholder.typicode.com/posts',function(err,response){ // url has to be of type string,this is the def of the callback
+// //Callback is Called inside the func you have passed it to, it's definition is the param
+
+// if(err){ // if error is returned. error is NULL when no error since NULL is what is passed as arg in the callback func call. 
+//     console.log(err)
+// }
+// else{
+// console.log(response) 
+// }
+// }) 
+
+
+// POST request
+
+
+
+// const data={
+//     title:'abcd',
+//     body:'the alphabet is cool'
+// }
+
+// lib.post('http://jsonplaceholder.typicode.com/posts',data,function(err,res){
+// if(err)
+// {
+//     console.log(err)
+// }else{
+//     console.log(res) // response of POST is the data we have sent. we get our sent data with the last id since it is added at the end
+                         // Proving that the data has been added successfully
+// }
+// })
+
+
+// PUT REQ
+
+
+// const updateData={
+//     title:'abcd',
+//     body:'the alphabet is cool'
+// }
+
+// lib.put('http://jsonplaceholder.typicode.com/posts/25',updateData,function(err,res){ // notice the 25. PUT will update the data with our data at id:25
+// if(err)
+// {
+//     console.log(err)
+// }else{
+//     console.log(res) // response is the 25th data updated with our data. Hence indicating successful update
+// }
+// })
+
+
+// DELETE request
+
+lib.del('http://jsonplaceholder.typicode.com/posts/25',function(err,res){
+    if(err)
+    {
+        console.log(err)
+    } else{
+        console.log(res) // response will be empty object but we have given a custom respone in the callback call 
+    }
+})
--- Creating AJAX library project -easyhhtp (ES5 version)/easyhttp.js
+++ Creating AJAX library project -easyhhtp (ES5 version)/easyhttp.js
@@ -1,11 +1,77 @@
  function easyHTTP(){
-     console.log(this)
+    
      this.http = new XMLHttpRequest() // we  need to use this here since it has to bind to whatever object when it is called using new
    
  }
 
+// GET request definition
+ easyHTTP.prototype.get=function(url,callback){  // prototype of the eassyHTTP object 
+// callback is used to perform some operation on a result in that very instance.
+//Instead of waiting for a return and then performing the operation. 
 
- easyHTTP.prototype.get=function(url){
-     console.log(2,this)
+     this.http.open('GET',url,true)
+         let self=this // binding this(referring to lib) to self because inside onload this will refer to XHRHTTP obj
+     this.http.onload=function(){
+        if(self.http.status===200){ // function called as soon as status = 200, response is passed as a param 
+            callback(null,self.http.responseText) // error param is null since this is the success part
+        }else{ // handling error
+            callback('Error :' + self.http.status)   // error param is passed since this the error part. 
+        }
+
+       
+     }
      
+     this.http.send()
  }
+
+ // POST request definition
+
+ easyHTTP.prototype.post=function(url,data,callback){ // data to be sent or POSTed 
+     this.http.open('POST',url,true)
+     this.http.setRequestHeader('Content-type','application/json') // tells the type of data returned
+        let self =this
+     this.http.onload=function(){
+          callback(null,self.http.responseText)
+     }
+
+
+     this.http.send(JSON.stringify(data))// since we are sending data we need to pass the data that we are sending. 
+     // data is in the js object form. needs to be converted to string before sending. because data exists in the browser in the form of strings
+ }
+
+
+ // PUT request Definition
+
+ easyHTTP.prototype.put=function(url,data,callback){ // data to be sent or updated
+    this.http.open('PUT',url,true)
+    this.http.setRequestHeader('Content-type','application/json') // tells the type of data returned
+       let self =this
+    this.http.onload=function(){
+         callback(null,self.http.responseText)
+    }
+
+
+    this.http.send(JSON.stringify(data))// since we are sending data we need to pass the data that we are sending. 
+    // data is in the js object form. needs to be converted to string before sending. because data exists in the browser in the form of strings
+}
+
+
+// DELETE request definition
+
+easyHTTP.prototype.del=function(url,callback){   
+   
+    
+         this.http.open('DELETE',url,true)
+             let self=this 
+         this.http.onload=function(){
+            if(self.http.status===200){ 
+                callback(null,'Post deleted') // When we delete data it returns an empty obj. Hence the custom text
+            }else{ 
+                callback('Error :' + self.http.status)  
+            }
+    
+           
+         }
+         
+         this.http.send()
+     }
